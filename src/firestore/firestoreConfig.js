bef1e6f41172cb5f2ba7ac63cca77f5eb4d6e311
unknown <eden.abdisa@yahoo.com>
2019-09-24 08:53:59 +0200

    Firebase file added

---
+++ src/firestore/firestoreConfig.js
@@ -0,0 +1,26 @@
+import app from 'firebase/app';
+import 'firebase/firestore';
+
+const config = {
+    apiKey: "AIzaSyA5EkxZ8ZTqE1OBwLarJaZ-2ljJAf_wtDo",
+    authDomain: "cleanaddis-bdaa1.firebaseapp.com",
+    databaseURL: "https://cleanaddis-bdaa1.firebaseio.com",
+    projectId: "cleanaddis-bdaa1",
+    storageBucket: "cleanaddis-bdaa1.appspot.com",
+    messagingSenderId: "871168571489",
+    appId: "1:871168571489:web:3b227f6f6403cb0c3e5220"
+}
+class Firebase{
+    constructor(){
+        app.initializeApp(config);
+        db = app.firestore();
+    }
+    render() {
+        address = () => this.db.collection('address');
+        return (
+            <p> {address} </p>
+        )
+    }   
+}
+export default Firebase;
+export default address;
