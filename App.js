b4d0b2fafa7569d3fb2cd8c5d6496890b48877c8
Amanuel Tadesse <aman.teferi.80@gmail.com>
2019-09-27 16:51:48 +0300

    Firebase fix trial #1

--- App.js
+++ App.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Firebase, address } from './src/firestore/firestoreConfig';
-
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import FirebaseComp from "./src/firestore/firebase";
 
 export default function App() {
-  return (
-      <View style={styles.container}>
-          <Firebase/>
-          <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<FirebaseComp />
+			<Text>Open up App.js to start working on your app!</Text>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center"
+	}
 });
